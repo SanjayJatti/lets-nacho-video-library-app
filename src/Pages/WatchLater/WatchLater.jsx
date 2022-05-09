3a59ca6468e81f75e3f752736ff48d48d4c085ee
SanjayJatti <jattisanjay.r@gmail.com>
2022-05-09 20:14:12 +0530

    Revert "feat: added header, sidebar and home screens"
    This reverts commit b800df9d31642018ffe50c1b6c3c1c68db83effe.

--- src/Pages/WatchLater/WatchLater.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const WatchLater = () => {
-  return (
-    <div>WatchLater</div>
-  )
-}
--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -1,70 +1,21 @@
+/**
+ * Videos Database can be added here.
+ * You can add videos of your wish with different attributes
+ * */
+
 export const videos = [
   {
-    _id: "TLnwqAarPkM",
-    category: "Bollywood",
-    title:
-      "Let’s Nacho - Kapoor & Sons | Sidharth | Alia | Fawad | Badshah",
+    _id: "Wo5dMEP_BbI",
+    title: "Awesome Video about Coding",
     description:
-      "After the smashing hit party song Kar Gayi Chull, Kapoor & Sons are back for round two with the next big dance track of the year - Let's Nacho! ",
-    channel: "Zee Music Marathi",
-    views: 996685,
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Soham Shah",
   },
   {
-    _id: "1zphUtx8oXg",
-    category: "Classical",
-    title: "Apeksha and Sadhvi ka Performance | Indias Best Dancer 2 ",
+    _id: "F_Riqjdh2oM",
+    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
+    creator: "Sentdex",
     description:
-      "Indias Best Dancer 2 will showcase the journey of some of the best talent who, with their solo performances, will compete to win the title",
-    channel: "SET India",
-    views: 456789,
-  },
-  {
-    _id: "7R7QJkznJGU",
-    category: "Lavani",
-    title:
-      "Wajle Ki Bara | Natarang HQ | Amruta Khanvilkar | Ajay-Atul",
-    description:
-      "Natarang, Indian Marathi film directed by Ravi Jadhav and starring Atul Kulkarni and Sonalee Kulkarni. Veteran music composer duo Ajay Atul composed the original score for the film",
-    channel: "Zee Music Marathi",
-    views: 956785,
-  },
-  {
-    _id: "Vf_bGg5SGz4",
-    category: "Classical",
-    title: "Classical Performance by Sadhvi | Indias Best Dancer 2  ",
-    description:
-      "Sadhwi comes in with yet another amazing classical dance performance with Aigiri Nandini",
-    channel: "SET India",
-    views: 606788,
-  },
-  {
-    _id: "mW67u_hWiSo",
-    category: "Lavani",
-    title:
-      "Apsara Aali Full Song | Natarang HQ | Marathi Songs",
-    description:
-      "Natarang, Indian Marathi film directed by Ravi Jadhav and starring Atul Kulkarni and Sonalee Kulkarni. Veteran music composer duo Ajay Atul composed the original score for the film",
-    channel: "Zee Music Marathi",
-    views: 946685,
-  },
-  {
-    _id: "k4yXQkG2s1E",
-    category: "Bollywood",
-    title:
-      "Kala Chashma | Baar Baar Dekho | Sidharth M Katrina K",
-    description:
-      "The song  is originally composed by Prem Hardeep, Kam Dhillon and recreated by Badshah, written by Amrik Singh and Kumaar and sung in the  widely loved voices of Amar Arshi, Badshah & Neha Kakkar.",
-    channel: "Zee Music Marathi",
-    views: 746685,
-  },
-  {
-    _id: "_lgcmYSePGA",
-    category: "Hip-Hop",
-    title:
-      "THE BOX - Roddy Ricch Dance Choreography | Matt Steffanina",
-    description:
-      "Matt Steffanina is a hip hop dance choreographer, DJ and producer from Los Angeles, CA. ",
-    channel: "Matt Steffanina",
-    views: 546685,
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
   },
 ];
