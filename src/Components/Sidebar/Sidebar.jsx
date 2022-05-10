57e7cef168b57e144fcd2f2ba2678b05467191a6
SanjayJatti <jattisanjay.r@gmail.com>
2022-05-10 09:20:43 +0530

    refactor

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -2,32 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
 
-const navlinkStyle = {
-  color: "var(--secondary-color",
-  fontWeight: "500",
-  fontSize: "inherit",
-  display: "flex",
-  flexDirection: "column"
-};
 export const Sidebar = () => {
   return (
     <div className="sidebar-container">
       <div className="sidebar-list-wrapper">
-        <Link to="/" style={navlinkStyle}>
+        <Link to="/" className="sidebar-link">
           {" "}
           <i className="fas fa-home"></i>Home
         </Link>
-        <Link to="/playlist" style={navlinkStyle}>
+        <Link to="/playlist" className="sidebar-link">
           <i className="fas fa-play-circle"></i>Playlist
         </Link>
-        <Link to="/history" style={navlinkStyle}>
+        <Link to="/history" className="sidebar-link">
           {" "}
           <i className="fas fa-history"></i>History
         </Link>
-        <Link to="/watchlater" style={navlinkStyle}>
+        <Link to="/watchlater" className="sidebar-link">
           <i className="fas fa-clock"></i>Watch Later{" "}
         </Link>
-        <Link to="/liked" style={navlinkStyle}>
+        <Link to="/liked" className="sidebar-link">
           <i className="fas fa-heart"></i>Liked
         </Link>
       </div>
