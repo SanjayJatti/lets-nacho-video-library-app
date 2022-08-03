86e697920cb752ae9a059a7187549f58ea9d90f3
SanjayJatti <jattisanjay.r@gmail.com>
2022-08-03 14:44:28 +0530

    docs : added videos info in database

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -1,9 +1,35 @@
 export const videos = [
   {
+    _id: "caoGNx1LF2Q",
+    category: "Bollywood",
+    title: "Ghagra Full Video Song",
+    description:
+      "Presenting the full video of the song Ghagra from the movie Yeh Jawaani Hai Deewani. Ghagra is an ethereal song based on folk balladry in the magical voice of Rekha Bharadwaj and Vishal Dadlani. The lyrics are penned by Amitabh Bhattacharya while music is composed by Pritam.",
+    channel: "T-Series",
+    views: 1936250,
+  },
+  {
+    _id: "JWhA3ldZcyY",
+    category: "Classical",
+    title: "Shiva Shambho:Bharatanatyam",
+    description:
+      "Dance adaptation of Parshwanath Upadhye's 'Shambho' in Ragam Bhatiyar & Palini and Talam Adi.Choreographed and performed by:Swathi Jaisankar,Isha Parupudi,Sophia Salingaros",
+    channel: "T-Series",
+    views: 2934250,
+  },
+  {
+    _id: "r6tU3GvJ5so",
+    category: "Lavani",
+    title: "Chandra Official Song",
+    description:
+      "Presenting biggest Marathi song of this season चंद्रा Chandra from most awaited Marathi movie 2022 Chandramukhi Sung by Shreya Ghoshal and composed by Ajay - Atul. Starring Amruta Khanvilkar, Addinath M Kothare",
+    channel: "Everest Marathi",
+    views: 5934260,
+  },
+  {
     _id: "TLnwqAarPkM",
     category: "Bollywood",
-    title:
-      "Let’s Nacho - Kapoor & Sons",
+    title: "Let’s Nacho - Kapoor & Sons",
     description:
       "After the smashing hit party song Kar Gayi Chull, Kapoor & Sons are back for round two with the next big dance track of the year - Let's Nacho! ",
     channel: "Sony Music India",
@@ -21,8 +47,7 @@ export const videos = [
   {
     _id: "7R7QJkznJGU",
     category: "Lavani",
-    title:
-      "Wajle Ki Bara | Natarang HQ",
+    title: "Wajle Ki Bara | Natarang HQ",
     description:
       "Natarang, Indian Marathi film directed by Ravi Jadhav and starring Atul Kulkarni and Sonalee Kulkarni. Veteran music composer duo Ajay Atul composed the original score for the film",
     channel: "Zee Music Marathi",
@@ -40,8 +65,7 @@ export const videos = [
   {
     _id: "mW67u_hWiSo",
     category: "Lavani",
-    title:
-      "Apsara Aali Full Song",
+    title: "Apsara Aali Full Song",
     description:
       "Natarang, Indian Marathi film directed by Ravi Jadhav and starring Atul Kulkarni and Sonalee Kulkarni. Veteran music composer duo Ajay Atul composed the original score for the film",
     channel: "Zee Music Marathi",
@@ -50,8 +74,7 @@ export const videos = [
   {
     _id: "k4yXQkG2s1E",
     category: "Bollywood",
-    title:
-      "Kala Chashma",
+    title: "Kala Chashma",
     description:
       "The song  is originally composed by Prem Hardeep, Kam Dhillon and recreated by Badshah, written by Amrik Singh and Kumaar and sung in the  widely loved voices of Amar Arshi, Badshah & Neha Kakkar.",
     channel: "Zee Music Compony",
@@ -60,11 +83,28 @@ export const videos = [
   {
     _id: "_lgcmYSePGA",
     category: "Hip-Hop",
-    title:
-      "THE BOX | Matt Steffanina",
+    title: "THE BOX | Matt Steffanina",
     description:
       "Matt Steffanina is a hip hop dance choreographer, DJ and producer from Los Angeles, CA. ",
     channel: "Matt Steffanina",
     views: 546685,
   },
+  {
+    _id: "aTTRJECOBOM",
+    category: "Hip-Hop",
+    title: "Mercy | Badshah",
+    description:
+      "Mercy | Badshah | Hip Hop | Naach Choreography | #DanceLikeAPro ",
+    channel: "Team Naach",
+    views: 7987935,
+  },
+  {
+    _id: "86DRJ_Y9prA",
+    category: "Hip-Hop",
+    title: "Shape Of You | Ed Sheeran",
+    description:
+      "Shape Of You | Ed Sheeran | Hip Hop Dance Routine by Sonali & Vijetha | LiveToDance with Sonali",
+    channel: "LiveToDance with Sonali",
+    views: 27135247,
+  }
 ];
